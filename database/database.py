fd220444492924e553424dd0abf54931bd28536d
Yasmim <arsegoyasmim@gmail.com>
2026-07-30 18:17:41 -0300

    Disable SQL echo

--- database/database.py
+++ database/database.py
@@ -9,7 +9,7 @@ from sqlmodel import SQLModel, create_engine
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
 if DATABASE_URL.startswith("postgresql://"):
     DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=not bool(os.getenv("DATABASE_URL")))
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
